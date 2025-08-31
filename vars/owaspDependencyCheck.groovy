d69f0982ad54950a9ea66cfbf5acadd3e462e64a
Prathamesh Patil <75564009+prathameshpatil7@users.noreply.github.com>
2025-08-31 18:29:53 +0530

    Update owaspDependencyCheck.groovy

--- vars/owaspDependencyCheck.groovy
+++ vars/owaspDependencyCheck.groovy
@@ -1,6 +1,6 @@
 def call(String OwaspEnv, String apiKeyCredId){
   withCredentials([string(credentialsId: apiKeyCredId, variable: 'NVD_API')]){
-    dependencyCheck additionalArguments: '--scan target/', odcInstallation: OwaspEnv
+    dependencyCheck additionalArguments: "--scan target/ --nvdApiKey ${NVD_KEY}", odcInstallation: OwaspEnv
     dependencyCheckPublisher pattern: "**/dependency-check-report.xml"
   }
 }
